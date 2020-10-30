73ab7e3d45d5f5e9bcf104cb6b63a6189d85b595
Max Liu <mjliu@mit.edu>
2020-10-30 10:48:49 -0400

    Update clean-data function to remove anonymous volumes
    Could be related to issues with resetting rabbitmq data

--- deploy.sh
+++ deploy.sh
@@ -258,8 +258,7 @@ diff-env() {
 
 clean-data() {
   echo "Cleaning up application data volumes..."
-  docker-compose stop app mongo nginx rabbit
-  docker-compose rm -f app mongo nginx rabbit
+  docker-compose rm -fsv app mongo nginx rabbit
   docker volume rm ${COMPOSE_PROJECT_NAME}_appdata
   docker volume rm ${COMPOSE_PROJECT_NAME}_staticdata
   echo "Clean up complete."
